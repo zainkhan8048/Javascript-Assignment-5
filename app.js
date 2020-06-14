9b591b532142af51a06eb66415563245649f98f5
zainkhan8048 <46112829+zainkhan8048@users.noreply.github.com>
2020-06-14 17:49:40 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,118 @@
+// ==============chapter: 1
+
+//===========Task:1
+// alert("Error!Please enter a password");
+
+//===========Task:2
+// alert("Welcome to JS land..\n Happy coding");
+
+//===========Task:3
+// alert("Welcome to JS land");
+
+//===========Task:4
+// alert("Happy coding");
+
+//===========Task:5
+// alert("Hello.... I can run JS through my web browser's console");
+
+
+
+
+// ==============Chapter: 2
+
+//===========Task:1
+// var username = "Zain";
+// alert(username);
+
+//===========Task:2
+// var myName = "Zain Zeeshan";
+// alert(myName);
+
+
+//===========Task:3
+// var message = "Hello World!!";
+// alert(message);
+
+
+//===========Task:4
+// var name = "Zain Zeeshan";
+// var age = "25 years old";
+// var course = "Certified Mobile App Development";
+// alert(name);
+// alert(age);
+// alert(course);
+
+
+
+//===========Task:5
+// var one = "PIZZA\n PIZZ\n PIZ\n PI\n P\n";
+// alert(one);
+
+
+//===========Task:6
+// var email = "My email address is zainkhan8048@gmail.com";
+// alert(email);
+
+
+//===========Task:7
+// var book = "A smarter way to learn JavaScript";
+// alert(book);
+
+
+
+//===========Task:8
+// var show = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”";
+// alert(show);
+
+
+
+// =================Chapter: 3
+
+
+//===========Task:1
+// var age = "I am 25yrs old";
+// alert(age);
+
+
+//===========Task:2
+// var age = "I am 25yrs old";
+// alert(age);
+
+
+
+// =================Chapter: 4
+
+//===========Task:1
+
+// document.write("A heading stating Rules for naming JS variables" + "<br>" + "Variable names can only contain letters, numbers, dollar sign and underscores");
+
+
+//===========Task:2
+// var [a, b, c, d] = [0, 1, 2, 3]
+// alert([a, b, c, d]);
+
+
+// =================Chapter: 5
+
+//===========Task:1
+
+// var y = 3 + 5;
+// alert(y);
+// document.write("Sum of 3 and 5 is 8");
+
+
+//===========Task:2
+// var y = 5 - 2;
+// alert(y);
+
+
+//===========Task:3
+// var y = 5 * 2;
+// alert(y);
+
+
+//===========Task:4
+// var y = 10 % 2;
+// alert(y);
+
+// =================Chapter: 6
